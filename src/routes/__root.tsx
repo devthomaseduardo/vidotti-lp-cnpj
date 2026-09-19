69cc3f400599630392a63c002cdec9df6879bc0d
thomasinoltra <thomas.nascimento@inoltra.tech>
2026-09-19 19:26:31 -0300

    feat: adiciona Google Tag Manager (GTM-MSF7R725)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -131,8 +131,22 @@ function RootShell({ children }: { children: ReactNode }) {
             />
           </>
         ) : null}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MSF7R725');`,
+          }}
+        />
       </head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MSF7R725" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
         {children}
         <Scripts />
       </body>
